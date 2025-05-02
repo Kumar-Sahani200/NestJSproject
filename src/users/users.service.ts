844995c38c6a4033012edcbd665eb17ca77ffec4
Kumar Sahani <ksahani@Lenovo.com>
2025-05-02 20:26:20 +0530

    initial commit

---
+++ src/users/users.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class UsersService {
+  private users = [
+    { id: 1, name: 'kumar', email: 'k@test.com', role: 'INTERN' },
+    { id: 2, name: 'Sumar', email: 's@test.com', role: 'ENGINEER' },
+    { id: 3, name: 'Bumar', email: 'b@test.com', role: 'ADMIN' },
+    { id: 4, name: 'cumar', email: 'c@test.com', role: 'INTERN' },
+  ];
+
+  findAll(role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
+    if (role) {
+      return this.users.filter((user) => user.role === role);
+    }
+    return this.users;
+  }
+
+  findOne(id: number) {
+    const user = this.users.find((user) => user.id === id);
+    return user;
+  }
+
+  create(user: {
+    name: string;
+    email: string;
+    role: 'INTERN' | 'ENGINEER' | 'ADMIN';
+  }) {
+    const userByHighestId = [...this.users].sort((a, b) => b.id - a.id);
+    const newUser = {
+      id: userByHighestId[0].id + 1,
+      ...user,
+    };
+  }
+}
